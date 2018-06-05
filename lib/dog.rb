8f76b6d114dac37d141947f3dcb62b0e5fedb15b
Sushi Chanrai <sushi@pathworks.co>
2018-06-05 19:36:33 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,19 @@
 # Your code goes here!
 class Dog 
    
-   def initialize(name)
-    @name = name
+   def initialize(new_name)
+    @name = new_name
   end
- 
+  
+   def name=(new_name)
+    @name = new_name
+  end
+  
   def name
     @name
   end
  
-  def name=(new_name)
-    @name = new_name
-  end
+
  
   def bark
    puts "woof!"
